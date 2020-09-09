c7ee9e8a66cff76c0ffc43095fcbb7dfd3e11f4b
EnginYıldırım <enginyldrm02@gmail.com>
2020-09-09 22:29:45 +0300

    Configuration file and Initial code added

---
+++ CoopSimulation/Animal.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoopSimulation
+{
+	public abstract class Animal
+	{
+
+		public Animal(IConfigurationSection configuration)
+		{
+
+		}
+	}
+}
---
+++ CoopSimulation/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace CoopSimulation
+{
+	
+	public class Program
+	{
+		public const string CONFIGNAME = "AnimalConfig";
+		public static IConfigurationRoot AppConfig { get; set; }
+		public static void Main(string[] args)
+		{
+			ServiceCollection serviceCollection = new ServiceCollection();
+			Initialize(serviceCollection);
+		}
+
+		public static void Initialize(IServiceCollection serviceCollection)
+		{
+
+			AppConfig = new ConfigurationBuilder()
+			   .SetBasePath(Directory.GetCurrentDirectory())
+			   .AddJsonFile("AppSettings.json", optional: true, reloadOnChange: true).Build();
+			//var configSection = AppConfig.GetSection(CONFIGNAME).AsEnumerable().ToDictionary(s => s.Key, v => v.Value);
+			var configSection = AppConfig.GetSection(CONFIGNAME);
+			serviceCollection.AddSingleton<IConfigurationSection>(configSection);
+
+			var serviceProvider = serviceCollection.BuildServiceProvider();
+
+
+		}
+	}
+}
